78f5f6a71bfbcebc055dc89d9ebca7967605c48d
Theo Chatzistavrou <teophysix@gmail.com>
2023-03-21 23:36:57 +0100

    latest customizer

--- HLTrigger/Configuration/python/customizeHLTforMixedPF.py
+++ HLTrigger/Configuration/python/customizeHLTforMixedPF.py
@@ -12,14 +12,14 @@ def customizeHLTForMixedPF(process):
     # allow maximum number of tracks to be sampled
     #process.hltIter0PFLowPixelSeedsFromPixelTracks.maxComplementTracks = cms.int32(500)
     # get tracks only close to first N vertices, by dz<0.3
-    process.hltIter0PFLowPixelSeedsFromPixelTracks.InputVertexCollectionForComplement = cms.InputTag("hltPixelVertices")
-    process.hltIter0PFLowPixelSeedsFromPixelTracks.maxComplementTrackVertex = cms.int32(30)
-    process.hltIter0PFLowPixelSeedsFromPixelTracks.dzMaxComplementTrackVertex = cms.double(0.3)
+   #process.hltIter0PFLowPixelSeedsFromPixelTracks.InputVertexCollectionForComplement = cms.InputTag("hltPixelVertices")
+    #process.hltIter0PFLowPixelSeedsFromPixelTracks.maxComplementTrackVertex = cms.int32(10)
+    #process.hltIter0PFLowPixelSeedsFromPixelTracks.dzMaxComplementTrackVertex = cms.double(0.3)
     
     # Apply cuts for pixel tracks complement
-
+ 
     ## |eta|<2.0 requirements --> quadraplets only to reduce timing (not very big impact in performance)
-    process.hltPixelTracksLowPTEta0To2 = cms.EDProducer( "TrackWithVertexSelector",
+    process.hltPixelTracksLowPT = cms.EDProducer( "TrackWithVertexSelector",
         normalizedChi2 = cms.double( 999999.0 ),
         numberOfValidHits = cms.uint32( 0 ),
         zetaVtx = cms.double( 0.3 ),
@@ -27,7 +27,7 @@ def customizeHLTForMixedPF(process):
         ptErrorCut = cms.double( 5.0 ),
         dzMax = cms.double( 999.0 ),
         etaMin = cms.double( 0.0 ),
-        etaMax = cms.double( 2.0 ),
+        etaMax = cms.double( 5.0 ),
         quality = cms.string( "highPurity" ), ##loose, tight, highPurity
         copyTrajectories = cms.untracked.bool( False ),
         nSigmaDtVertex = cms.double( 0.0 ),
@@ -46,39 +46,11 @@ def customizeHLTForMixedPF(process):
         useVtx = cms.bool( False ) ## Turning off vertex selection
     )
 
-    ## |eta|>2.0 requirements --> triplets/quadraplets with highPurity
-    process.hltPixelTracksLowPTEta2To5 = cms.EDProducer( "TrackWithVertexSelector",
-        normalizedChi2 = cms.double( 999999.0 ),
-        numberOfValidHits = cms.uint32( 0 ),
-        zetaVtx = cms.double( 0.3 ),
-        rhoVtx = cms.double( 0.2 ),
-        ptErrorCut = cms.double( 5.0 ),
-        dzMax = cms.double( 999.0 ),
-        etaMin = cms.double( 2.0 ),
-        etaMax = cms.double( 5.0 ),
-        quality = cms.string( "highPurity" ), ##loose, tight, highPurity
-        copyTrajectories = cms.untracked.bool( False ),
-        nSigmaDtVertex = cms.double( 0.0 ),
-        timesTag = cms.InputTag( "" ),
-        ptMin = cms.double( 0.0 ), # minimum pT cut --> try 1.,2. GeV to reduce tracks
-        ptMax = cms.double( 6.0 ), # maximum pT cut
-        d0Max = cms.double( 999.0 ),
-        copyExtras = cms.untracked.bool( False ),
-        nVertices = cms.uint32( 2 ),
-        vertexTag = cms.InputTag( "hltPixelVertices" ),
-        src = cms.InputTag( "hltIter0PFLowPixelSeedsFromPixelTracks" ), # the complement reco::TrackCollection
-        vtxFallback = cms.bool( True ),
-        numberOfLostHits = cms.uint32( 999 ),
-        numberOfValidPixelHits = cms.uint32( 3 ),
-        timeResosTag = cms.InputTag( "" ),
-        useVtx = cms.bool( False ) ## Turning off vertex selection
-    )
-
     
 
 
 
-    process.HLTIterativeTrackingIteration0 = cms.Sequence(process.hltIter0PFLowPixelSeedsFromPixelTracks+process.hltPixelTracksLowPTEta0To2+process.hltPixelTracksLowPTEta2To5+process.hltIter0PFlowCkfTrackCandidates+process.hltIter0PFlowCtfWithMaterialTracks+process.hltIter0PFlowTrackCutClassifier+process.hltMergedTracks)
+    process.HLTIterativeTrackingIteration0 = cms.Sequence(process.hltIter0PFLowPixelSeedsFromPixelTracks+process.hltPixelTracksLowPT+process.hltIter0PFlowCkfTrackCandidates+process.hltIter0PFlowCtfWithMaterialTracks+process.hltIter0PFlowTrackCutClassifier+process.hltMergedTracks)
 
 
     # Merging Iter0 tracks with the complement of pixel tracks
@@ -90,13 +62,13 @@ def customizeHLTForMixedPF(process):
         Epsilon = cms.double( -0.001 ),
         MaxNormalizedChisq = cms.double( 1000.0 ),
         MinFound = cms.int32( 3 ),
-        TrackProducers = cms.VInputTag( 'hltPixelTracksLowPTEta0To2','hltPixelTracksLowPTEta2To5','hltPFMuonMerging' ),
-        hasSelector = cms.vint32( 0, 0 , 0),
-        indivShareFrac = cms.vdouble( 1.0, 1.0, 1.0),
-        selectedTrackQuals = cms.VInputTag( 'hltPixelTracksLowPTEta0To2','hltPixelTracksLowPTEta2To5','hltPFMuonMerging' ),
+        TrackProducers = cms.VInputTag( 'hltPixelTracksLowPT','hltPFMuonMerging' ),
+        hasSelector = cms.vint32( 0, 0),
+        indivShareFrac = cms.vdouble( 1.0, 1.0),
+        selectedTrackQuals = cms.VInputTag( 'hltPixelTracksLowPT','hltPFMuonMerging' ),
         setsToMerge = cms.VPSet(
           cms.PSet(  pQual = cms.bool( False ),
-            tLists = cms.vint32( 0, 1, 2)
+            tLists = cms.vint32( 0, 1)
           )
         ),
         trackAlgoPriorityOrder = cms.string( "hltESPTrackAlgoPriorityOrder" ),
@@ -130,3 +102,4 @@ def customizeHLTForMixedPF(process):
     process.HLTTrackingForBeamSpot = cms.Sequence(process.HLTPreAK4PFJetsRecoSequence+process.HLTL2muonrecoSequence+process.HLTL3muonrecoSequence+process.HLTDoLocalPixelSequence+process.HLTRecopixelvertexingSequence+process.HLTDoLocalStripSequence+process.HLTIterativeTrackingIter02+process.hltPFMuonMerging+process.hltPFTracks)
     
     return process
+
