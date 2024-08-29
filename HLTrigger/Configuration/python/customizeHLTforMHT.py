52bf9838533f7d196ecdf46c2625afb634052ad1
Theo Chatzistavrou <teophysix@gmail.com>
2024-08-29 12:31:41 +0200

    automatically track the producers

--- HLTrigger/Configuration/python/customizeHLTforMHT.py
+++ HLTrigger/Configuration/python/customizeHLTforMHT.py
@@ -1,13 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 def customizeHLTforMHT(process):
-  process.hltPFMHTNoMuTightID.minEtaFwdJetMht = cms.double(4.7)
-  process.hltPFMHTNoMuTightID.minPtFwdJetMht = cms.double(30.)
-
-  process.hltPFMHTNoMuTightIDHFCleaned.minEtaFwdJetMht = cms.double(4.7)
-  process.hltPFMHTNoMuTightIDHFCleaned.minPtFwdJetMht = cms.double(30.)
-
-  process.hltPFMHTTightID.minEtaFwdJetMht = cms.double(4.7)
-  process.hltPFMHTTightID.minPtFwdJetMht = cms.double(30.)
+  # find automatically all HLT HT/MHT producers
+  for prod in producers_by_type(process, "HLTHtMhtProducer"):
+    prod.minEtaFwdJetMht = cms.double(4.7)
+    prod.minPtFwdJetMht = cms.double(30.)
 
   return process
