71171da737bdb3446b1aab1390682a6c5a4278f6
Theo Chatzistavrou <57989988+theochatzis@users.noreply.github.com>
2024-11-15 15:46:14 +0200

    Update customizeHLTforMHT.py

--- HLTrigger/Configuration/python/customizeHLTforMHT.py
+++ HLTrigger/Configuration/python/customizeHLTforMHT.py
@@ -4,7 +4,7 @@ from HLTrigger.Configuration.common import producers_by_type
 def customizeHLTforMHT(process):
   # find automatically all HLT HT/MHT producers
   for prod in producers_by_type(process, "HLTHtMhtProducer"):
-    prod.minEtaFwdJetMht = cms.double(4.7)
+    prod.minEtaFwdJetMht = cms.double(3.0)
     prod.minPtFwdJetMht = cms.double(30.)
 
   return process
